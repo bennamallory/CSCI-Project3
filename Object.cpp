fd3ef6ad2cd339cfd561c0f7a8c8277fbffc089d
bennamallory <46735177+bennamallory@users.noreply.github.com>
2020-04-01 13:05:21 -0700

    Create Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,39 @@
+// CS1300 Spring 2020
+// Author: Mallory Benna and Srvya Dhanwanda
+// Recitation: 106 – Sravanth Yajamanam
+// Project 3 - Object Class Source File
+
+#include <iostream> 
+#include "Object.h"
+using namespace std; 
+
+//Default constructor
+Object::Object(){
+    objectName = "";
+    objectValue = 0;
+}
+
+//Parameterized constructor
+Object::Object(string objectName_, string objectValue_){
+    objectName = objectName_;
+    objectValue = objectValue_;
+}
+
+
+//Getters
+string Object::getObjectName(){
+    return objectName;
+}
+
+int Object::getObjectValue(){
+    return objectValue;
+}
+
+//Setters
+void Object::setObjectName(objectName_){
+    objectName = objectName_;
+}
+
+void Object::setObjectValue(objectValue_){
+    objectValue = objectValue_;
+}
