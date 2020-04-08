2a9cbe6b7e91b6ef359f4e182c12e38bcc1235cc
bennamallory <46735177+bennamallory@users.noreply.github.com>
2020-04-08 12:02:18 -0700

    Comments

--- Object.cpp
+++ Object.cpp
@@ -1,5 +1,5 @@
-// CSC1300 Spring 2020
-// Author: Mallory Benna and Sravya Dhanwanda
+// CS1300 Spring 2020
+// Author: Mallory Benna and Srvya Dhanwanda
 // Recitation: 106 – Sravanth Yajamanam
 // Project 3 - Object Class Source File
 
@@ -19,20 +19,44 @@ Object::Object(string objectName_, int objectValue_){
     objectValue = objectValue_;
 }
 
-//Getters
+
+/*
+* This function gets the name of the object
+* Parameters: none
+* Return: string objectName
+*/
 string Object::getObjectName(){
     return objectName;
 }
 
+
+
+/*
+* This function gets the value of the object
+* Parameters: none
+* Return: int objectValue
+*/
 int Object::getObjectValue(){
     return objectValue;
 }
 
-//Setters
+
+
+/*
+* This function sets the name of the object
+* Parameters: string objectName_
+* Return: none
+*/
 void Object::setObjectName(string objectName_){
     objectName = objectName_;
 }
 
+
+/*
+* This function sets the value of the object
+* Parameters: int objectValue_
+* Return: none
+*/
 void Object::setObjectValue(int objectValue_){
     objectValue = objectValue_;
 }
