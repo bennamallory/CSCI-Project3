6e5634b17ca031b694f223dd773d74b254660453
bennamallory <46735177+bennamallory@users.noreply.github.com>
2020-04-07 18:30:29 -0600

    Fix compile errors

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,8 @@
+// CS1300 Spring 2020
+// Author: Mallory Benna and Srvya Dhanwanda
+// Recitation: 106 – Sravanth Yajamanam
+// Project 3 - Player Class Source File
+
 #include<iostream>
 #include <string>
 using namespace std;
@@ -10,27 +15,43 @@ Player::Player(){
     money=0;
     characterScore=0;
 }
+
+
 void Player:: setName(string name_){
     name=name_;
 }
+
+
 string Player:: getName(){
     return name;
 }
+
+
 void Player:: setIntelligence(int intelligence_){
     intelligence=intelligence_;
 }
+
+
 int Player:: getIntelligence(){
     return intelligence;
 }
+
+
 void Player::setMoney(double money_){
-    money=money_;
+    money = money_;
 }
+
+
 double Player::getMoney(){
     return money;
 }
-void Player:: setCharacterScore(characterScore_){
-    characterScore=characterScore_;
+
+
+void Player:: setCharacterScore(double characterScore_){
+    characterScore = characterScore_;
 }
+
+
 double Player::getCharacterScore(){
     return characterScore;
 }
