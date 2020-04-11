fe13f3e307ef715f50d6d0726b8003cc6e91aa63
sravya-dhano <63015698+sravya-dhano@users.noreply.github.com>
2020-04-10 20:34:31 -0600

    updates getter and setter on Strength

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,5 @@
-// CS1300 Spring 2020
-// Author: Mallory Benna and Srvya Dhanwanda
+// CSC1300 Spring 2020
+// Author: Mallory Benna and Sravya Dhanwanda
 // Recitation: 106 – Sravanth Yajamanam
 // Project 3 - Player Class Source File
 
@@ -8,7 +8,6 @@
 using namespace std;
 #include "Player.h"
 
-//Default constructor
 Player::Player(){
     name="";
     intelligence=0;
@@ -17,76 +16,48 @@ Player::Player(){
     characterScore=0;
 }
 
-//Parameterized constructor
+
 void Player:: setName(string name_){
-    name = name_;
+    name=name_;
 }
 
-/*
-* This function gets the name of the player
-* Parameters: none
-* Return: string name
-*/
+
 string Player:: getName(){
     return name;
 }
 
 
-/*
-* This function sets the intelligence of the player
-* Parameters: int intelligence_
-* Return: none
-*/
 void Player:: setIntelligence(int intelligence_){
-    intelligence = intelligence_;
+    intelligence=intelligence_;
 }
 
 
-/*
-* This function gets the intelligence of the player
-* Parameters: none
-* Return: int intelligence
-*/
 int Player:: getIntelligence(){
     return intelligence;
 }
 
 
-/*
-* This function sets the money of the player
-* Parameters: double money_
-* Return: none
-*/
 void Player::setMoney(double money_){
     money = money_;
 }
 
 
-
-/*
-* This function gets the money of the player
-* Parameters: none
-* Return: double money
-*/
 double Player::getMoney(){
     return money;
 }
 
+void Player:: setStrength(int strength_){
+    strength=strength_;
+}
 
-/*
-* This function sets the character score of the player
-* Parameters: double characterScore_
-* Return: none
-*/
+int Player::getStrength(){
+    return strength;
+}
 void Player:: setCharacterScore(double characterScore_){
     characterScore = characterScore_;
 }
 
-/*
-* This function gets the character score of the player
-* Parameters: none
-* Return: double characterScore_
-*/
+
 double Player::getCharacterScore(){
     return characterScore;
 }
